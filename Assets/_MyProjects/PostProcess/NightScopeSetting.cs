ec7aa7bbffd05ff8e7d1598807032378a21561f4
井上敦登 <inoue-atsuto@kayac.com>
2019-06-26 23:39:12 +0900

    とりあえずカスタムPostEffectできた

---
+++ Assets/_MyProjects/PostProcess/NightScopeSetting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[System.Serializable]
+[PostProcess(typeof(NightScopeRenderer), PostProcessEvent.AfterStack, "atsuhan/PostProcess/NightScope")]
+public sealed class NightScopeSetting : PostProcessEffectSettings
+{
+  public ColorParameter color1 = new ColorParameter { value = Color.gray };
+  public ColorParameter color2 = new ColorParameter { value = Color.gray };
+  public ColorParameter color3 = new ColorParameter { value = Color.gray };
+  public ColorParameter color4 = new ColorParameter { value = Color.gray };
+
+  private Texture2D _paletteTexture;
+  public Texture2D PaletteTexture
+  {
+    get
+    {
+      Color[] color = new Color[] { color1, color2, color3, color4 };
+      if (_paletteTexture == null)
+      {
+        _paletteTexture = new Texture2D(color.Length, 1, TextureFormat.ARGB32, false);
+        _paletteTexture.filterMode = FilterMode.Point;
+      }
+      _paletteTexture.SetPixels(color);
+      _paletteTexture.Apply();
+      return _paletteTexture;
+    }
+  }
+  public FloatParameter paletteWidth = new FloatParameter { };
+}
---
+++ Assets/_MyProjects/PostProcess/PostProcess_NightScope.shader
@@ -0,0 +1,37 @@
+﻿Shader "atsuhan/PostProcess/NightScope"
+{
+	HLSLINCLUDE
+
+	#include "PostProcessing/Shaders/StdLib.hlsl"
+
+	TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
+	TEXTURE2D_SAMPLER2D(_PaletteTex, sampler_PaletteTex);
+	sampler3D _DitherMaskLOD;
+	float _PaletteWidth;
+
+	float4 Frag(VaryingsDefault i) : SV_Target
+	{
+		float3 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord);
+		float grayScale = dot(color, float3(0.299, 0.587, 0.114));
+		half dither = tex3D(_DitherMaskLOD, float3(i.texcoord * _ScreenParams.xy / 4, frac(grayScale * (_PaletteWidth - 1)) * 0.9375)).a;
+		color = SAMPLE_TEXTURE2D(_PaletteTex, sampler_PaletteTex, float2(grayScale / _PaletteWidth * (_PaletteWidth - 1) + dither / _PaletteWidth, 0.5));
+		return float4(color, 1);
+	}
+
+	ENDHLSL
+
+	SubShader
+	{
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			HLSLPROGRAM
+
+			#pragma vertex VertDefault
+			#pragma fragment Frag
+
+			ENDHLSL
+		}
+	}
+}

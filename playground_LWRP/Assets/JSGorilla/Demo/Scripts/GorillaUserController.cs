9226387d8d492ebad6b33f0d784cbebc15b367a2
井上敦登 <inoue-atsuto@kayac.com>
2019-11-13 18:53:26 +0900

    add Gorilla  animation

---
+++ playground_LWRP/Assets/JSGorilla/Demo/Scripts/GorillaUserController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GorillaUserController : MonoBehaviour {
+	GorillaCharacter gorillaCharacter;
+	
+	void Start () {
+		gorillaCharacter = GetComponent < GorillaCharacter> ();
+	}
+	
+	void Update () {	
+		if (Input.GetButtonDown ("Fire1")) {
+			gorillaCharacter.Attack();
+		}
+		if (Input.GetButtonDown ("Jump")) {
+			gorillaCharacter.Jump();
+		}
+		if (Input.GetKeyDown (KeyCode.H)) {
+			gorillaCharacter.Hit();
+		}
+		if (Input.GetKeyDown (KeyCode.K)) {
+			gorillaCharacter.Death();
+		}
+		if (Input.GetKeyDown (KeyCode.L)) {
+			gorillaCharacter.Rebirth();
+		}		
+		if (Input.GetKeyDown (KeyCode.R)) {
+			gorillaCharacter.Gallop();
+		}				
+		if (Input.GetKeyUp (KeyCode.R)) {
+			gorillaCharacter.Walk();
+		}		
+		if (Input.GetKeyDown (KeyCode.U)) {
+			gorillaCharacter.Drum();
+		}	
+
+		gorillaCharacter.forwardSpeed= gorillaCharacter.walkMode*Input.GetAxis ("Vertical");
+		gorillaCharacter.turnSpeed= Input.GetAxis ("Horizontal");
+	}
+}
